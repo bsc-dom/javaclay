cd627135fba090a2441ff46ab815cc4025474568
dgasull <dgasull@bsc.es>
2020-06-17 14:18:40 +0200

    Preparing release

--- .travis/new_version.sh
+++ .travis/new_version.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then 
+	 git fetch
+     git checkout master
      git add pom.xml
-	 git commit -m "New release"
+	 git commit -m "Modified pom.xml"
 	 git push origin HEAD:$TRAVIS_BRANCH
 
 	 ## update develop branch also ##
 	 git fetch	 
 	 git checkout develop
      git add pom.xml
-	 git commit -m "New release"
+	 git commit -m "Modified pom.xml"
 	 git push origin HEAD:develop
 else
      echo "Skipping new version tag because current branch is not master";
