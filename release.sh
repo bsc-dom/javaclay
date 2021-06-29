f6a008fdb0f9c5e1d0523e8eabc792f67e617b55
dgasull <dgasull@bsc.es>
2021-06-29 11:52:58 +0200

    Preparing release

--- release.sh
+++ release.sh
@@ -36,10 +36,6 @@ done
 
 POM_VERSION=$(grep version pom.xml | grep -v -e '<?xml|~'| head -n 1 | sed 's/[[:space:]]//g' | sed -E 's/<.{0,1}version>//g' | awk '{print $1}')
 printMsg "Welcome to javaClay release script"
-GIT_BRANCH=$(git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \$2}")
-if [[ "$GIT_BRANCH" != "$BRANCH_TO_CHECK" ]]; then
-  printError "Branch is not $BRANCH_TO_CHECK. Found $GIT_BRANCH. Aborting script"
-fi
 
 if [ "$PROMPT" = true ]; then
   read -p "Version defined is $POM_VERSION. Is this ok? (y/n) " -n 1 -r
